b2566f52dfe6d229d89a2429c87df69ab6f21850
Sagar Dhadke <peaceunique2003@gmail.com>
2024-11-12 21:04:47 +0530

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:internet_connection_getx/connectivity_controller.dart';
 import 'package:internet_connection_getx/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //return our connectivity controller
+  Get.put(ConnectivityController());
+
   runApp(const MyApp());
 }
 
@@ -11,14 +18,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Internet Connection GetX',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyHome()
-    );
+    return GetMaterialApp(
+        title: 'Internet Connection GetX',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyHome());
   }
 }
